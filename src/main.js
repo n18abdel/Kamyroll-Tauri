7f161535e6ba2e5db10edff2208418000a4cd811
Bilnaa <noufnouf703@gmail.com>
2022-10-26 00:06:56 +0200

    add a shield where if the program is in fullscreen when it's not intended it will go back to window mode

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import 'vue-global-api/computed';
 import 'vue-global-api/watch';
 import 'vue-global-api/watchEffect';
 import { getToken,testToken, generateNewToken } from './scripts/token';
+import { appWindow } from '@tauri-apps/api/window';
 
 let token = await getToken();
 
@@ -28,6 +29,10 @@ if (localStorage.getItem('token_valid') < Date.now()) {
     }
 }
 
+if(appWindow.isFullscreen() && !window.location.href.includes('watch')){
+    await appWindow.setFullscreen(false);
+    console.log('no more fullscreen');
+}
 
 const app = createApp(App);
 
