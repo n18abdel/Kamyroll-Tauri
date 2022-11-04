43cddf9cfd423629594b94fe6db3bf38951dddaa
Bilnaa <noufnouf703@gmail.com>
2022-11-04 23:42:49 +0100

    [skip ci] deleted unused components and added a timeout for the trending component (if anilist is down)

--- src/pages/home/home-page.vue
+++ src/pages/home/home-page.vue
@@ -1,14 +1,3 @@
-<script setup>
-
-// import Counter from '../../components/counter.vue';
-// import ColorChecker from '../../components/color-checker.vue';
-// import Requester from '../../components/requester.vue';
-// import { moduleExpression } from '@babel/types';
-// import Requester from '../../components/requester.vue';
-import building from '../../components/building.vue';
-import DarkModeToggle from '../../components/DarkModeToggle.vue';
-
-</script>
 <template>  
         <trending></trending>   
         <LastEpisodes></LastEpisodes>
@@ -17,7 +6,9 @@ import DarkModeToggle from '../../components/DarkModeToggle.vue';
 <script>
 // load the component before the page is rendered
 import { defineAsyncComponent } from 'vue';
-const Trending = defineAsyncComponent(() => import('../../components/tendance.vue'));
+const Trending = defineAsyncComponent(() => import('../../components/tendance.vue'),{
+    timeout : 1000,
+});
 
 // load lastepisodes only when trending is loaded
 const LastEpisodes = defineAsyncComponent(() => import('../../components/lastepisodes.vue'), {
@@ -25,6 +16,17 @@ const LastEpisodes = defineAsyncComponent(() => import('../../components/lastepi
   timeout: 3000,
   suspend: true
 })
+
+
+export default {
+  components: {
+    Trending,
+    LastEpisodes
+  },
+  mounted: async function () {
+    console.log(Trending.$data)
+  }
+}
 
 
 </script>
