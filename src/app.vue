dfb0a6b704289b85d2acb95b5b96b62f8106d661
Bilnaa <noufnouf703@gmail.com>
2022-12-04 02:30:24 +0100

    added a version and channel indicator

--- src/app.vue
+++ src/app.vue
@@ -10,23 +10,35 @@ import banner from './components/banner.vue'
        <router-view/>
     </v-main>
   </v-app>
+  <footer class="app-footer-wrapper">
+    <div class="erc-footer">
+       <div class="content">
+          <div class="footer-copyright">
+            <div class="copyright">{{version}} - Kamyroll</div>
+          </div>
+       </div>
+    </div>
+    
+  </footer>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      version : '',
+    }
+  },
   methods: {
     generateNewToken(){
       localStorage.clear();
     }
   },
   mounted : function (){
+    this.version = 'v'+process.env.APP_VERSION.replaceAll('"','') + ' - ' + process.env.CHANNEL.replaceAll('"','');
     document.addEventListener('keydown', (event) => {
-      // ctrl + t
-      if (event.ctrlKey && event.key === 't') {
-        this.generateNewToken();
-      }
-      // cmd + t 
-      if (event.metaKey && event.key === 't') {
+      // ctrl + t  || cmd + t => generate new token
+      if (event.ctrlKey && event.key === 't' || event.metaKey && event.key === 't') {
         this.generateNewToken();
       }
     });
