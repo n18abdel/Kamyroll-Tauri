4e155472127acaec5eb70f74619b6cccd0f67801
Bilnaa <noufnouf703@gmail.com>
2022-12-17 23:34:51 +0100

    [skip ci] moved the token verification to the app component This will allow us to put a snack bar so the user have live feedback

--- src/app.vue
+++ src/app.vue
@@ -9,6 +9,9 @@ import banner from './components/banner.vue'
     <v-main>
        <router-view/>
     </v-main>
+    <v-snackbar v-model="snackbar" :timeout="timeout" :bottom="true" :right="true">
+      {{ text }}
+    </v-snackbar>
   </v-app>
   <footer class="app-footer-wrapper">
     <div class="erc-footer">
@@ -18,24 +21,58 @@ import banner from './components/banner.vue'
           </div>
        </div>
     </div>
-    
   </footer>
 </template>
 
 <script>
+ import { testToken, generateNewToken } from './scripts/token';
 export default {
   data() {
     return {
-      version : '',
+      version: '',
+      snackbar: false,
+      text: '',
+      timeout: 2000,
     }
   },
   methods: {
-    generateNewToken(){
+    generateNewToken() {
       localStorage.clear();
     }
   },
-  mounted : function (){
-    this.version = 'v'+process.env.APP_VERSION.replaceAll('"','') + ' - ' + process.env.CHANNEL.replaceAll('"','');
+  mounted: async function () {
+    let token_valid = localStorage.getItem('token_valid');
+    let currentDate = Math.floor(new Date().getTime() / 1000);
+
+    if (localStorage.getItem('miniProgressBar') == null) {
+      localStorage.setItem('miniProgressBar', 'false');
+    }
+    if (Number(token_valid) < currentDate) {
+      console.log('Testing the token validity');
+      let result = await testToken(localStorage.getItem('token'));
+      if (result == false) {
+        console.log('Token is invalid, generating new token');
+        this.text = 'Token is invalid, generating new token';
+        if (this.snackbar = true) {
+          this.snackbar = false;
+        }
+        this.snackbar = true;
+        await generateNewToken();
+        window.location.reload();
+      } else {
+        console.log('token is still valid, adding 6 hours to the token_valid date');
+        this.text = 'token is still valid, adding 6 hours to the token_valid date';
+        if (this.snackbar = true) {
+          this.snackbar = false;
+        }
+        this.snackbar = true;
+        let curDatePlusSix = new Date();
+        curDatePlusSix.setHours(curDatePlusSix.getHours() + 6);
+        curDatePlusSix = Math.floor(curDatePlusSix.getTime() / 1000);
+        localStorage.setItem('token_valid', curDatePlusSix);
+      }
+    }
+    this.version = 'v' + process.env.APP_VERSION.replaceAll('"', '') + ' - ' + process.env.CHANNEL.replaceAll('"', '');
     document.addEventListener('keydown', (event) => {
       // ctrl + t  || cmd + t => generate new token
       if (event.ctrlKey && event.key === 't' || event.metaKey && event.key === 't') {
