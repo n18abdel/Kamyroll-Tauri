b4e2e5359e8e8428330b9af518c25ad2bb04ff0e
Bilnaa <noufnouf703@gmail.com>
2022-12-18 01:33:45 +0100

    [skip ci] vuetify stuff

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,19 @@
-import 'vuetify/styles';
-
-
-import { createVuetify } from 'vuetify';
-
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 
 export default createVuetify({
-  theme: {
-    defaultTheme: 'dark'
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
   }
-});
+})
