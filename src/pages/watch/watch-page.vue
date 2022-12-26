86e63f564ec3681fe7ceac8060c19fd6a96a6b35
Bilnaa <noufnouf703@gmail.com>
2022-12-27 00:00:24 +0100

    [skip ci] fixed an issue with poster images that would crash the page

--- src/pages/watch/watch-page.vue
+++ src/pages/watch/watch-page.vue
@@ -7,10 +7,11 @@
             <Artplayer v-else @get-instance="getInstance" :option="option" :subs="subs" :videos="videos" :info="metadat" />
             <div class="video-content" >
                 <div class="content" v-if="loaded">
-                    <div class="media-metadata"><a class="poster-image-wrapper" :href="metadat.url"  v-if="Object.keys(metadat.title_info.images).length > 1"><span
-                                class="poster-hover-layer">to
-                                series</span><img :src="metadat.title_info.images?.poster_tall[image].source" class="c-content-image"
-                                :alt="metadat.title"></a>
+                    <div class="media-metadata">
+                        <a class="poster-image-wrapper" :href="metadat.url" >
+                            <span class="poster-hover-layer">to series</span>
+                            <img v-if="Object.keys(metadat.images).length > 1" :src="metadat.images?.poster_tall[image].source" class="c-content-image" :alt="metadat.title">
+                        </a>
                         <div class="text-wrapper"><a class="episode-info" :href="metadat.url"><span class="series"><svg
                                         class="left-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 20"
                                         data-t="left-arrow-svg">
@@ -110,15 +111,13 @@ export default {
     mounted: async function () {
         try {
             this.metadat = await getMetadata(this.id);
-            var id = this.metadat.series_id == undefined ? this.id : this.metadat.series_id;
+            var id = this.metadat.series_id == undefined ? this.metadat.listing_id : this.metadat.series_id;
             var meta1 = await getMetadata(id);
-            this.metadat.__type__ = meta1.__class__;
             this.metadat.title_info = meta1;
+            this.metadat.__type__ = meta1.__class__;
             this.metadat.url = `/${channel.replace('-','')}/` + meta1.id;
-            if(Object.keys(this.metadat.title_info.images).length == 1 && this.metadat.title_info.images.poster_tall == undefined){
-                this.metadat.title_info.images.poster_tall = this.metadat.images.thumbnail;
-            }
-            this.image = Math.floor((this.metadat.title_info.images.poster_tall.length - 1) / 2);
+            this.metadat.images.poster_tall = meta1.images.poster_tall;
+            this.image = Math.floor(this.metadat.images.poster_tall.length / 2 - 1);
             let sources = await getVideos(this.id);
             this.videos = sources.streams;
             this.subs = sources.subs;
