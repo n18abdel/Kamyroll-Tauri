49bd8e8e0db34bca6c9b70fb86376bf3bf8d9274
Bilnaa <noufnouf703@gmail.com>
2022-12-28 15:41:53 +0100

    the hitbox on the setting button is now the whole tile not only the image and change the background color

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -90,16 +90,14 @@ import kamyroll_logo from '/img/kamyroll_logo.svg';
               </li>
             </ul>
           </li>
-          <li class="user-actions-item">
+          <li class="user-actions-item" @click="dialog == true ? dialog = false : dialog = true" >
                 <div class="erc-header-tile">
+                  <div class="erc-header-svg">
+                          <svg class="header-svg-icon" aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.738 10H22V14H19.739C19.498 14.931 19.1 15.798 18.565 16.564L20 18L18 20L16.565 18.564C15.797 19.099 14.932 19.498 14 19.738V22H10V19.738C9.069 19.498 8.203 19.099 7.436 18.564L6 20L4 18L5.436 16.564C4.901 15.799 4.502 14.932 4.262 14H2V10H4.262C4.502 9.068 4.9 8.202 5.436 7.436L4 6L6 4L7.436 5.436C8.202 4.9 9.068 4.502 10 4.262V2H14V4.261C14.932 4.502 15.797 4.9 16.565 5.435L18 3.999L20 5.999L18.564 7.436C19.099 8.202 19.498 9.069 19.738 10ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"></path></svg>
+                  </div>
                   <div class="text-center">
                     <v-dialog v-model="dialog">
-                      <template v-slot:activator="{ props }">
-                        <div class="erc-header-svg" v-bind="props">
-                          <svg class="header-svg-icon" aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.738 10H22V14H19.739C19.498 14.931 19.1 15.798 18.565 16.564L20 18L18 20L16.565 18.564C15.797 19.099 14.932 19.498 14 19.738V22H10V19.738C9.069 19.498 8.203 19.099 7.436 18.564L6 20L4 18L5.436 16.564C4.901 15.799 4.502 14.932 4.262 14H2V10H4.262C4.502 9.068 4.9 8.202 5.436 7.436L4 6L6 4L7.436 5.436C8.202 4.9 9.068 4.502 10 4.262V2H14V4.261C14.932 4.502 15.797 4.9 16.565 5.435L18 3.999L20 5.999L18.564 7.436C19.099 8.202 19.498 9.069 19.738 10ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"></path></svg>
-                        </div>
-                      </template>
-                      <v-card>
+                      <v-card style="background-color: #1b1a26; color: white">
                         <v-card-text>
                           SETTINGS
                         </v-card-text>
@@ -129,7 +127,7 @@ import kamyroll_logo from '/img/kamyroll_logo.svg';
                           ></v-switch>
                         </v-card-text>
                         <v-card-actions>
-                          <v-btn color="black" block @click="dialog = false">Close Dialog</v-btn>
+                          <v-btn color="white" block @click="dialog = false">Close Dialog</v-btn>
                         </v-card-actions>
                       </v-card>
                     </v-dialog>
