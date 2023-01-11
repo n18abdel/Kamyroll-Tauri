facc063140247f9b3111405cda231b45032e1518
Bilnaa <noufele.mehdi@hotmail.fr>
2023-01-11 02:15:55 +0100

    [skip ci] adn label is shorter for the search part

--- src/scripts/channel_id.js
+++ src/scripts/channel_id.js
@@ -39,7 +39,7 @@ var chan = channel;
 
 const channels = reactive([
     { name: CRUNCHYROLL, short_label:'Crunchyroll',label: 'Crunchyroll',icon:crunchyroll_logo,description : 'Crunchyroll is an American distributor, publisher and licensing company focused on streaming anime, manga, drama, and more.',color:'#ff6600'},
-    { name: ANIMEDIGITALNETWORK, short_label:'ADN', label: 'Animation Digital Network', icon: adn_logo,description : 'Animation Digital Network is a French distributor, publisher and licensing company focused on streaming anime and manga.',color:'#0095ff' },
+    { name: ANIMEDIGITALNETWORK, short_label:'ADN', label: 'ADN', icon: adn_logo,description : 'Animation Digital Network is a French distributor, publisher and licensing company focused on streaming anime and manga.',color:'#0095ff' },
     { name: NEKO_SAMA, short_label:'Neko-sama', label: 'Neko-sama',icon: nekosama_logo ,description : 'Neko-sama is a french streaming website focused on streaming anime',color:'#1cb9f4' }
 ]);
 
