7ce09ae18278e6e54ce6ac7b02976d99a10fd0e4
Bilnaa <noufele.mehdi@hotmail.fr>
2023-01-16 23:17:13 +0100

    fixed the splashscreen timing

--- src/app.vue
+++ src/app.vue
@@ -48,11 +48,13 @@ import banner from './components/banner.vue'
  import {
    invoke
  } from '@tauri-apps/api/tauri';
- import {appWindow} from '@tauri-apps/api/window';
+ import {
+   appWindow
+ } from '@tauri-apps/api/window';
  export default {
    data() {
      return {
-       emits : ['toast','error'],
+       emits: ['toast', 'error'],
        version: 'v' + process.env.APP_VERSION.replaceAll('"', '') + ' - ' + process.env.CHANNEL.replaceAll('"', ''),
        snackbar: false,
        text: '',
@@ -60,18 +62,18 @@ import banner from './components/banner.vue'
      }
    },
    methods: {
-      log(message){
-        console.log(message);
-      },
-      toast(message){
-        this.snackbar = false;
-        this.text = message;
-        console.log(message);
-        this.snackbar = true;
-        setTimeout(() => {
-          this.snackbar = false;
-        }, this.timeout);
-      },
+     log(message) {
+       console.log(message);
+     },
+     toast(message) {
+       this.snackbar = false;
+       this.text = message;
+       console.log(message);
+       this.snackbar = true;
+       setTimeout(() => {
+         this.snackbar = false;
+       }, this.timeout);
+     },
      async ctrlt() {
        let status = await generateNewToken();
        if (status == true) {
@@ -81,47 +83,46 @@ import banner from './components/banner.vue'
          this.toast('Token is not generated, please try again. (' + status + ')');
        }
      },
-     async sleep(ms){
-        return new Promise(resolve => setTimeout(resolve, ms));
+     async sleep(ms) {
+       return new Promise(resolve => setTimeout(resolve, ms));
      }
    },
    mounted: async function () {
-     setInterval(async ()=>{
-       await invoke('close_splashscreen')
+     await this.sleep(3500).then(async () => {
+       await invoke('close_splashscreen');
        await appWindow.show();
-     },15000)
-     
+     });
      let token_expire = localStorage.getItem('token_expire');
      let token_valid = Number(localStorage.getItem('token_valid'));
      let currentDate = Math.floor(new Date().getTime() / 1000);
-
      if (localStorage.getItem('miniProgressBar') == null) {
        localStorage.setItem('miniProgressBar', 'false');
-     } else if (localStorage.getItem('autoplay') == null){
+     } else if (localStorage.getItem('autoplay') == null) {
        localStorage.setItem('autoplay', 'false');
      }
 
      if (token_expire != null) {
-        if (currentDate > token_expire) {
-          await generateNewToken();
-          this.toast('Token is expired, generating new token')
-          window.location.reload();
-        } else if (currentDate > token_valid) {
-          let test = await testToken(localStorage.getItem('token'));
-          if (test == false) {
-            await generateNewToken();
-            this.toast('Token is not working anymore, generating new token');
-            window.location.reload();
-          } else {
-            localStorage.setItem('token_valid', currentDate + 21600);
-            this.toast('Token is valid, but will be tested again in 6 hours')
-          }
-        }
+       if (currentDate > token_expire) {
+         await generateNewToken();
+         this.toast('Token is expired, generating new token')
+         window.location.reload();
+       } else if (currentDate > token_valid) {
+         let test = await testToken(localStorage.getItem('token'));
+         if (test == false) {
+           await generateNewToken();
+           this.toast('Token is not working anymore, generating new token');
+           window.location.reload();
+         } else {
+           localStorage.setItem('token_valid', currentDate + 21600);
+           this.toast('Token is valid, but will be tested again in 6 hours')
+         }
+       }
+
      } else {
        await generateNewToken();
      }
 
-     if(localStorage.getItem('discord_rpc') == null) {
+     if (localStorage.getItem('discord_rpc') == null) {
        localStorage.setItem('discord_rpc', 'true');
      } else if (localStorage.getItem('discord_rpc') == 'false') {
        await invoke('disable_rpc');
